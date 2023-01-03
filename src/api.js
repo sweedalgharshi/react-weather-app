d517f20d39c9308b272305f86f99464f8a3a004d
sweedalgharshi <sweedalgracius@gmail.com>
2023-01-03 19:33:30 +0530

    part 1 of forecast component and styling and adding accordion

--- src/api.js
+++ src/api.js
@@ -9,3 +9,9 @@ export const geoApiOptions = {
 
 export const GEO_API_URL =
   'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL =
+  'https://api.openweathermap.org/data/2.5/';
+
+export const WEATHER_API_KEY =
+  '3cfafe233a1c612c812db0b1177cfe14';
---
+++ src/components/forecast/forecast.js
@@ -0,0 +1,63 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemButton,
+  AccordionItemHeading,
+  AccordionItemPanel,
+} from 'react-accessible-accordion';
+
+import './forecast.css';
+
+const WEEKS_DAYS = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
+
+const Forecast = ({ data }) => {
+  const dayInAWeek = new Date().getDay();
+  const forecastDays = WEEKS_DAYS.slice(
+    dayInAWeek,
+    WEEKS_DAYS.length
+  ).concat(WEEKS_DAYS.slice(0, dayInAWeek));
+
+  return (
+    <>
+      <label className="title">Daily</label>
+      <Accordion allowZeroExpanded>
+        {data.list.splice(0, 7).map((item, index) => (
+          <AccordionItem key={index}>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img
+                    alt="weather"
+                    className="icon-small"
+                    src={`icons/${item.weather[0].icon}.png`}
+                  />
+                  <label className="day">
+                    {forecastDays[index]}
+                  </label>
+                  <label className="description">
+                    {item.weather[0].description}
+                  </label>
+                  <label className="min-max">
+                    {Math.round(item.main.temp_min)}°C /{' '}
+                    {Math.round(item.main.temp_max)}°C
+                  </label>
+                </div>
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel></AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
+  );
+};
+
+export default Forecast;
