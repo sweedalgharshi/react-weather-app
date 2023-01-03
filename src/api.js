717ce1880e7fe617b747d85bb7d37424cb21f437
sweedalgharshi <sweedalgracius@gmail.com>
2023-01-03 13:41:04 +0530

    Added Search Component Along with fetch request for country

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key':
+      '347061c1b7msh6f3a66f4dc8d694p1dd62cjsne612600b4cb0',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+};
+
+export const GEO_API_URL =
+  'https://wft-geo-db.p.rapidapi.com/v1/geo';
